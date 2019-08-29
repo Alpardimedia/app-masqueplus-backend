18adce4ef926e086fc5eb3baafe4c0c522078394
Alberto Pardo Díaz <albertopardo21690@gmail.com>
2019-08-29 23:40:08 +0200

    Sistema de login con Google

--- config/config.js
+++ config/config.js
@@ -7,3 +7,9 @@ process.env.PORT = process.env.PORT || 3000;
 // Semilla para generar el token
 // ==============================================================
 module.exports.SEED = 'starwars-monkey-iloveyou-whatever-margaretthatcheris110-unodostrescuatro';
+
+// ==============================================================
+// Google
+// ==============================================================
+module.exports.GOOGLE_CLIENT_ID = '612930200355-vguoe6jsgc03sqff8rb6uu0v8dhig7es.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'MhDsQIjM2bVDtxl3ELepWs41';
